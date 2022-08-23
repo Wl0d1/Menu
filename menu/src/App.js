30517fdd1eda8986e526c1ea747687de0ea98bd1
Tomasz <tomas_wlodarczyk@o2.pl>
2022-08-23 14:34:48 +0200

    Add header

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from './header/header.js'
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <main>
+    <section>
+      <Header />
+    </section></main>
+  
+  </div>;
 }
 
 export default App;
---
+++ menu/src/header/header.js
@@ -0,0 +1,12 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div>
+        <h1> Our menu</h1>
+      <p className="underline"></p>
+    </div>
+  );
+};
+
+export default Header;
